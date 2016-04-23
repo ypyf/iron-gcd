780c8436e83c209dcfb8eee9223e36581afa968f
ypyf <t34@qq.com>
2016-04-24 02:16:34 +0800

    format code

--- src/main.rs
+++ src/main.rs
@@ -89,11 +89,13 @@ fn post_gcd(request: &mut Request) -> IronResult<Response> {
     Ok(response)
 }
 
-fn gcd(mut m : u64, mut n : u64) -> u64 {
+fn gcd(mut m: u64, mut n: u64) -> u64 {
     assert!(m != 0 && n != 0);
     while m != 0 {
         if m < n {
-            let t = m; m = n; n = t;
+            let t = m;
+            m = n;
+            n = t;
         }
         m = m % n;
     }
